bb4f1b08c64a3e6b9a942fb06b718d5bc90dbee1
voidchecksam <146993325+voidchecksam@users.noreply.github.com>
2026-04-10 06:21:04 +0500

    init: setting up the baseline with google genai

---
+++ screenshot analyzer/main.py
@@ -0,0 +1,15 @@
+import pynput
+from PIL import ImageGrab
+from plyer import notification
+import json
+from google import genai
+
+# using pynput and plyer for windows
+# trying out google's shiny new genai.
+client = genai.Client(api_key="API_F**KING_KEY_HERE")
+
+def main():
+    print("starting desktop vision assistant on gemini 2.5...")
+
+if __name__ == "__main__":
+    main()
